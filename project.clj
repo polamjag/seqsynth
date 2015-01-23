7d921a7b8bc5a40e03d23fc19d67b90ae22e3da9
polamjag <s@polamjag.info>
2015-01-23 14:11:15 +0900

    initial

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject seqsynth "0.1.0-SNAPSHOT"
+  :description "sequenther with ADSR"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
---
+++ src/seqsynth/core.clj
@@ -0,0 +1,7 @@
+(ns seqsynth.core)
+
+(defn getval
+  "get current mod value"
+  [seqs seq_max time params adsr]
+
+  )
